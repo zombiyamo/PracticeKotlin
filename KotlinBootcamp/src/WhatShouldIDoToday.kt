019d0cee19a889f0f663a16d9a23def13d202cea
zombiyamo <ars.amnoskmkz@gmail.com>
2025-01-21 22:40:03 +0900

    update whatShouldIDoToday

--- KotlinBootcamp/src/WhatShouldIDoToday.kt
+++ KotlinBootcamp/src/WhatShouldIDoToday.kt
@@ -1,14 +1,29 @@
 fun main() {
-    println(whatShouldIDoToday("happy"))
-    println(whatShouldIDoToday("sad"))
-    println(whatShouldIDoToday("happy", "cloudy"))
-    println(whatShouldIDoToday("happy", temperature = 20))
+    print("Type your mood: ")
+    val mood = readlnOrNull().toString()
+    print(whatShouldIDoToday(mood))
 }
 
 fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
-    return if (mood == "happy" && weather == "sunny" && temperature in 20..30) {
-        "go for a walk"
-    } else {
-        "Stay home and read."
+    return when {
+        shouldStayInBed(mood, weather, temperature) ->
+            "stay in bed"
+
+        shouldGoForAWalk(mood, weather, temperature) ->
+            "go for a walk"
+
+        shouldGoSwimming(temperature) ->
+            "go swimming"
+
+        else ->
+            "Stay home and read."
     }
 }
+
+fun shouldStayInBed(mood: String, weather: String, temperature: Int) =
+    mood == "sad" && weather == "rainy" && temperature == 0
+
+fun shouldGoForAWalk(mood: String, weather: String, temperature: Int) =
+    mood == "happy" && weather == "sunny" && temperature in 20..30
+
+fun shouldGoSwimming(temperature: Int) = temperature > 35
