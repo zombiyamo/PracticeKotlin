758a232a4c97c90245c4ab6ee0cee01fe25949c1
zombiyamo <ars.amnoskmkz@gmail.com>
2025-04-11 21:15:00 +0900

    SpecialClasses.ktを追加

---
+++ KotlinBootcamp/src/SpecialClasses.kt
@@ -0,0 +1,16 @@
+package aquarium
+
+object MobyDickWhale {
+    val author = "Herman Melville"
+
+    fun jump() {
+        println("Herman Melville")
+    }
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
+
